d73537c86dcb1e031ff4e713fdc6af5b1052966c
KarenHippitaku <hippitaku@gmail.com>
2019-05-30 14:13:56 -0500

    Firestore configuration active. Merge with Alex master.

---
+++ src/firestore.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+    apiKey:  "AIzaSyDe5Oi89gh8ErB9F66j6VDx2kpAMVMtg-g",
+    authDomain: "openhouse-e0266.firebaseapp.com",
+    projectId: 'openhouse-e0266'
+  });
+  
+  // Initialize Cloud Firestore through Firebase
+  var db = firebase.firestore();
